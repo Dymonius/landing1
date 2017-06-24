c2ac599b1d148799dacc197db8e83150cd7a592f
Dmitry <dymonius12@gmail.com>
2017-06-24 21:41:07 +0300

    Form validation and navigation was done

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,38 @@
-(function () {
-    //Add event listner for open form button
+(function() {
     var openFormButton = document.querySelector('.arrow-down');
+    var form = document.querySelector('.form');
+    var nav = document.querySelector('.nav');
 
     if (openFormButton) {
-        openFormButton.addEventListener('click', function (e) {
+        openFormButton.addEventListener('click', function(e) {
             e.preventDefault();
-            form.open()
+            LANDING.form.open();
         })
     }
+
+    if (form) {
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            if (LANDING.form.isValid()) {
+                console.log('All good');
+            } else {
+                console.log('Is not valid');
+            }
+
+        })
+    }
+
+    if (nav) {
+        nav.addEventListener('click', function(e) {
+            var target = e.target;
+
+            if (target.tagName.toLowerCase() !== 'a') {
+                return;
+            }
+
+            e.preventDefault();
+            LANDING.navigation.toggleToActiveLink(target);
+        });
+    }
+
 }());
